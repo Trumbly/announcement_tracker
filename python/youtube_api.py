929c72472905b2bf16ff35088affe089a484ccbd
HannesLadendorf <hannes.ladendorf@gmx.de>
2023-08-15 17:01:00 +0200

    youtubeserach response persitance funktioniert

--- python/youtube_api.py
+++ python/youtube_api.py
@@ -7,7 +7,7 @@ from typing import List, Dict, Optional
 import datetime
 from sqlalchemy import create_engine, Integer, String, DateTime, ForeignKey, Column
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, DeclarativeBase, Relationship
+from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, DeclarativeBase, Relationship, relationship
 
 #Base = declarative_base()
 class Base(DeclarativeBase):
@@ -19,22 +19,22 @@ class Base(DeclarativeBase):
 #     width: int
 #     height: int
 
-@dataclass
-class Snippet(Base):
-    __tablename__ = "snippets"
+# @dataclass
+# class Snippet(Base):
+#     __tablename__ = "snippets"
 
-    pk = Column(Integer, primary_key=True, autoincrement=True)
+#     pk = Column(Integer, primary_key=True, autoincrement=True)
 
-    publishedAt = Column(DateTime)
-    channelId = Column(String)
-    title = Column(String)
-    description = Column(String)
-    #thumbnails: Dict[str, Thumbnails]
-    channelTitle = Column(String)
-    liveBroadcastContent = Column(String)
+#     publishedAt = Column(DateTime)
+#     channelId = Column(String)
+#     title = Column(String)
+#     description = Column(String)
+#     #thumbnails: Dict[str, Thumbnails]
+#     channelTitle = Column(String)
+#     liveBroadcastContent = Column(String)
 
-    searchResourceId_fk = Column(Integer, ForeignKey("search_resource_id.pk"))
-    searchResourceId = Relationship("SearchResourceId", back_populates="snippets")
+#     searchResourceId_fk: Mapped[int] = mapped_column(ForeignKey("search_resource_id.pk")) #Column(Integer, ForeignKey("search_resource_id.pk"))
+    # searchResourceId: Mapped["SearchResourceId"] = relationship(back_populates="snippets") #Relationship("SearchResourceId", back_populates="snippets")
 
 
     # def __init__(self, pubushedAt, channelId, title, description, channelTitle, liveBroadcastContent):
@@ -45,51 +45,90 @@ class Snippet(Base):
     #     self.channelTitle = channelTitle
     #     self.liveBroadcastContent = liveBroadcastContent
 
-    #searchRessourceId_fk:Mapped[int] = mapped_column(ForeignKey("search_ressource_id.pk"))
-    #searchRessourceId: Mapped["SearchRessourceId"] = relationship(back_populates="snippets")
+    
+# @dataclass
+# class SearchResourceId(Base):
+#     __tablename__ = "search_resource_id"
+
+#     pk = Column(Integer, primary_key=True, autoincrement=True)
+
+#     kind = Column(String)
+#     videoId = Column(String)
+#     channelId = Column(String)
+#     playlistId = Column(String)
+#     snippets = Relationship("Snippet", back_populates="searchResourceId", uselist=True)  #List[Snippet]
+
+#     searchResource_fk: Mapped[int] = mapped_column(ForeignKey("search_resources.pk")) #Column(Integer, ForeignKey("search_resources.pk"))
+#     searchResource: [Mapped["SearchResource"]] = relationship(back_populates="id") #Relationship("SearchResource", back_populates="id")
+
+
+# @dataclass
+# class SearchResource(Base):
+#     __tablename__ = "search_resources"
+
+#     pk = Column(Integer, primary_key=True, autoincrement=True)
+
+#     kind = Column(String)
+#     etag = Column(String)
+#     # id: Mapped[List["SearchResourceId"]] = relationship(back_populates="searchResource")#Relationship("SearchResourceId", back_populates="searchResourceId", uselist=True)  #List[SearchResourceId]
+
+#     response_fk: Mapped[int] =  mapped_column(ForeignKey("response.pk")) #Column(Integer, ForeignKey("responses.pk"))
+#     response: Mapped["Response"] = relationship(back_populates="items")#Relationship("Response", back_populates="items")
+    
 
 @dataclass
-class SearchResourceId:
-    __tablename__ = "search_resource_id"
+class PageInfo(Base):
+    __tablename__ = "page_infos"
+
+    pk = Column(Integer, primary_key=True, autoincrement=True)
+
+    totalResults = Column(Integer)
+    resultsPerPage = Column(Integer)
+
+    response_fk = Column(Integer, ForeignKey("responses.pk")) #: Mapped[int] = mapped_column(ForeignKey("responses.pk"))
+    response = Relationship("Response", back_populates="pageInfos")#: Mapped["Response"] = relationship(back_populates="pageInfos")
+    
+    def __repr__(self) -> str:
+        return (f"PageInfo(totalResults={self.totalResults}, resultsPerPage={self.resultsPerPage})")
+
+    def __init__(self, totalResults, resultsPerPage):
+        self.totalResults = totalResults
+        self.resultsPerPage = resultsPerPage
+        print(self)
+
+
+@dataclass
+class Response(Base):
+    __tablename__ = "responses"
 
     pk = Column(Integer, primary_key=True, autoincrement=True)
 
     kind = Column(String)
-    videoId = Column(String)
-    channelId = Column(String)
-    playlistId = Column(String)
-    snippets = Relationship("Snippet", back_populates="searchResourceId", uselist=True)  #List[Snippet]
-
-    searchResource_fk = Column(Integer, ForeignKey("search_resource.pk"))
-    searchResource = Relationship("SearchResource", back_populates="id")
-
-@dataclass
-class SearchResource:
-    __tablename__ = "search_resource"
-
-    pk = Column(Integer, primary_key=True, autoincrement=True)
-
-    kind:str
     etag = Column(String)
-    id = Relationship("SearchResourceId", back_populates="searchResourceId", uselist=True)  #List[SearchResourceId]
+    nextPageToken = Column(String)
+    # prevPageToken = Column(String) #TODO prevPageToken seems to be missing in API response
+    regionCode = Column(String)
+    pageInfos = Relationship("PageInfo", back_populates="response")#: Mapped[List["PageInfo"]] = relationship(back_populates="response") #List[PageInfo]
+    # items: Mapped[List["SearchResource"]] = relationship(back_populates="response")#Relationship("SearchResource", back_populates="response")#List[SearchResource]
 
+    def __repr__(self):
+        return (f"Response(kind={self.kind}, etag={self.etag}, nextPageToken={self.nextPageToken}, regionCode={self.regionCode}, pageInfos={self.pageInfos})")
+
+    def __init__(self, kind, etag, nextPageToken, regionCode, pageInfo, items):
+        self.kind = kind
+        self.etag = etag
+        self.nextPageToken = nextPageToken
+        self.regionCode = regionCode
+        self.pageInfo = pageInfo
+        self.items = items
+        print(self)
     
+    def __post_init__(self): #https://www.youtube.com/watch?v=Fu0swCLAJ8E
+        self.pageInfos = [PageInfo(**pageInfo) for pageInfo in self.pageInfos]
+        print(self)
+    #     session.add(self)
 
-@dataclass
-class PageInfo:
-    totalResults: int
-    resultsPerPage: int
 
-
-@dataclass
-class Response:
-    kind: str
-    etag: str
-    nextPageToken: str
-    #prevPageToken: str #TODO prevPageToken seems to be missing in API response
-    regionCode: str
-    pageInfo: List[PageInfo]
-    items: List[SearchResource]
 
 @dataclass
 class Search:
@@ -165,7 +204,8 @@ class Search:
         request_response = search_request.execute()
         self.response = Response(**request_response)
 
-        #session.add(self.response)
+
+        session.add(self.response)
         session.commit()
         session.close()
 
@@ -173,4 +213,3 @@ class Search:
 
 search = Search(q="airport ambiance noise", type = "video", eventType = "completed")
 search.execute()
-print(search)
