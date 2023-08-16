3007283f0da9856eb29ac908ebcc8c94a1ee5f15
HannesLadendorf <hannes.ladendorf@gmx.de>
2023-08-16 23:38:40 +0200

    youtube api search.list request and persitence

--- python/youtube_api.py
+++ python/youtube_api.py
@@ -3,11 +3,8 @@ from dotenv import load_dotenv
 import googleapiclient.discovery
 import googleapiclient.errors
 from dataclasses import dataclass
-from typing import List, Dict, Optional
-import datetime
-from sqlalchemy import create_engine, Integer, String, DateTime, ForeignKey, Column, event
-# from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, declarative_base, DeclarativeBase, Relationship, relationship, mapper, Mapper, scoped_session
+from sqlalchemy import create_engine, Integer, String, DateTime, ForeignKey, Column, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base, Relationship
 
 load_dotenv() #load environment variables from .env file
 
@@ -28,151 +25,188 @@ session = Session()
 #     width: int
 #     height: int
 
-# @dataclass
-# class Snippet(Base):
-#     __tablename__ = "snippets"
+@dataclass
+class Snippet(Base):
+    __tablename__ = "snippets"
 
-#     pk = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    publishedAt = Column(DateTime)
+    publishTime = Column(DateTime)
+    channelId = Column(String)
+    title = Column(String)
+    description = Column(String)
+    #thumbnails: Dict[str, Thumbnails]
+    channelTitle = Column(String)
+    liveBroadcastContent = Column(String)
+    searchResource_id = Column(Integer, ForeignKey("search_resources.id"))
+    searchResource = Relationship("SearchResource", back_populates="snippet")
 
-#     publishedAt = Column(DateTime)
-#     channelId = Column(String)
-#     title = Column(String)
-#     description = Column(String)
-#     #thumbnails: Dict[str, Thumbnails]
-#     channelTitle = Column(String)
-#     liveBroadcastContent = Column(String)
 
-#     searchResourceId_fk: Mapped[int] = mapped_column(ForeignKey("search_resource_id.pk")) #Column(Integer, ForeignKey("search_resource_id.pk"))
-    # searchResourceId: Mapped["SearchResourceId"] = relationship(back_populates="snippets") #Relationship("SearchResourceId", back_populates="snippets")
-
-
-    # def __init__(self, pubushedAt, channelId, title, description, channelTitle, liveBroadcastContent):
-    #     self.publishedAt = pubushedAt
-    #     self.channelId = channelId
-    #     self.title = title
-    #     self.description = description
-    #     self.channelTitle = channelTitle
-    #     self.liveBroadcastContent = liveBroadcastContent
+    def __init__(self, publishedAt, channelId, title, description, channelTitle, liveBroadcastContent, publishTime, thumbnails):
+        self.publishedAt = publishedAt
+        self.channelId = channelId
+        self.title = title
+        self.description = description
+        self.channelTitle = channelTitle
+        self.liveBroadcastContent = liveBroadcastContent
+        self.publishTime = publishTime
 
     
-# @dataclass
-# class SearchResourceId(Base):
-#     __tablename__ = "search_resource_id"
+@dataclass
+class SearchResourceId(Base):
+    __tablename__ = "search_resource_id"
 
-#     pk = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    kind = Column(String)
+    videoId = Column(String, nullable=True)
+    channelId = Column(String, nullable=True)
+    playlistId = Column(String, nullable=True)
 
-#     kind = Column(String)
-#     videoId = Column(String)
-#     channelId = Column(String)
-#     playlistId = Column(String)
-#     snippets = Relationship("Snippet", back_populates="searchResourceId", uselist=True)  #List[Snippet]
+    searchResource_id = Column(Integer, ForeignKey("search_resources.id"))
+    searchResource = Relationship("SearchResource", back_populates="searchResourceId")
 
-#     searchResource_fk: Mapped[int] = mapped_column(ForeignKey("search_resources.pk")) #Column(Integer, ForeignKey("search_resources.pk"))
-#     searchResource: [Mapped["SearchResource"]] = relationship(back_populates="id") #Relationship("SearchResource", back_populates="id")
+    def __init__(self, kind, videoId, channelId=None, playlistId=None):
+        self.kind=kind
+        self.videoId = videoId
+        self.channelId = channelId
+        self.playlistId = playlistId
 
 
-# @dataclass
-# class SearchResource(Base):
-#     __tablename__ = "search_resources"
+@dataclass
+class SearchResource(Base):
+    __tablename__ = "search_resources"
 
-#     pk = Column(Integer, primary_key=True, autoincrement=True)
-
-#     kind = Column(String)
-#     etag = Column(String)
-#     # id: Mapped[List["SearchResourceId"]] = relationship(back_populates="searchResource")#Relationship("SearchResourceId", back_populates="searchResourceId", uselist=True)  #List[SearchResourceId]
-
-#     response_fk: Mapped[int] =  mapped_column(ForeignKey("response.pk")) #Column(Integer, ForeignKey("responses.pk"))
-#     response: Mapped["Response"] = relationship(back_populates="items")#Relationship("Response", back_populates="items")
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    kind = Column(String)
+    etag = Column(String)
+    searchResourceId = Relationship("SearchResourceId", back_populates="searchResource", uselist=False)
+    snippet = Relationship("Snippet", back_populates="searchResource", uselist=False) 
+    response_id = Column(Integer, ForeignKey("responses.id"))
+    response = Relationship("Response", back_populates="searchResources", )
     
+    def __repr__(self) -> str:
+        return(f"SearchResource(kind={self.kind}, etag={self.etag})")
+
+    def __init__(self, kind, etag, id, snippet):
+        self.kind = kind
+        self.etag = etag
+        self.snippet = Snippet(**snippet)
+        self.searchResourceId= SearchResourceId(**id)
+
 
 @dataclass
 class PageInfo(Base):
     __tablename__ = "page_infos"
 
-    pk = Column(Integer, primary_key=True, autoincrement=True)
-
+    id = Column(Integer, primary_key=True, autoincrement=True)
     totalResults = Column(Integer)
     resultsPerPage = Column(Integer)
-
-    response_fk = Column(Integer, ForeignKey("responses.pk")) #: Mapped[int] = mapped_column(ForeignKey("responses.pk"))
-    response = Relationship("Response", back_populates="pageInfo")#: Mapped["Response"] = relationship(back_populates="pageInfos")
-    
-    def __repr__(self) -> str:
-        return (f"PageInfo(totalResults={self.totalResults}, resultsPerPage={self.resultsPerPage})")
+    response_id = Column(Integer, ForeignKey("responses.id"))
+    response = Relationship("Response", back_populates="pageInfo")
 
     def __init__(self, totalResults, resultsPerPage):
         self.totalResults = totalResults
         self.resultsPerPage = resultsPerPage
-        print(self)
 
 
 @dataclass
 class Response(Base):
     __tablename__ = "responses"
-
-    pk = Column(Integer, primary_key=True, autoincrement=True)
-
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
     kind = Column(String)
     etag = Column(String)
     nextPageToken = Column(String)
-    # prevPageToken = Column(String) #TODO prevPageToken seems to be missing in API response
+    prevPageToken = Column(String, nullable=True)
     regionCode = Column(String)
-    pageInfo = Relationship("PageInfo", back_populates="response", uselist=False)#: Mapped[List["PageInfo"]] = relationship(back_populates="response") #List[PageInfo]
-    # items: Mapped[List["SearchResource"]] = relationship(back_populates="response")#Relationship("SearchResource", back_populates="response")#List[SearchResource]
+    pageInfo = Relationship("PageInfo", back_populates="response", uselist=False)
+    searchResources= Relationship("SearchResource", back_populates="response")
+    search_id = Column(Integer, ForeignKey("searches.id"))
+    search = Relationship("Search", back_populates="response")
 
-    def __repr__(self):
-        return (f"Response(kind={self.kind}, etag={self.etag}, nextPageToken={self.nextPageToken}, regionCode={self.regionCode}, pageInfo={self.pageInfo})")
-
-    def __init__(self, kind, etag, nextPageToken, regionCode, pageInfo, items):
+    def __init__(self, kind, etag, nextPageToken, regionCode, pageInfo, items, prevPageToken=None):
         self.kind = kind
         self.etag = etag
         self.nextPageToken = nextPageToken
+        self.prevPageToken = prevPageToken
         self.regionCode = regionCode
-        self.pageInfo = PageInfo(**pageInfo) #for pi in pageInfo]
-        self.items = items
+        self.pageInfo = PageInfo(**pageInfo)
+        self.searchResources = [SearchResource(**item) for item in items]
 
 
 @dataclass
-class Search:
+class Search(Base):
+    __tablename__ = "searches"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    part = Column(String)
+    forContentOwner = Column(Boolean, nullable=True)
+    forDeveloper = Column(Boolean, nullable=True)
+    forMine = Column(Boolean, nullable=True)
+    relatedToVideoId = Column(String, nullable=True)
+    channelId = Column(String, nullable=True)
+    channelType = Column(String, nullable=True)
+    eventType = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    locationRadius = Column(String, nullable=True)
+    maxResults = Column(Integer, nullable=True)
+    onBehalfOfContentOwner = Column(String, nullable=True)
+    order = Column(String, nullable=True)
+    pageToken = Column(String, nullable=True)
+    publishedAfter = Column(String, nullable=True)
+    publishedBefore = Column(String, nullable=True)
+    q = Column(String, nullable=True)
+    regionCode = Column(String, nullable=True)
+    relevanceLanguage = Column(String, nullable=True)
+    safeSearch = Column(String, nullable=True)
+    topicId = Column(String, nullable=True)
+    type = Column(String, nullable=True)
+    videoCaption = Column(String, nullable=True)
+    videoCategoryId = Column(String, nullable=True)
+    videoDefinition = Column(String, nullable=True)
+    videoDimension = Column(String, nullable=True)
+    videoDuration = Column(String, nullable=True)
+    videoEmbeddable = Column(String, nullable=True)
+    videoLicense = Column(String, nullable=True)
+    videoSyndicated = Column(String, nullable=True)
+    videoType = Column(String, nullable=True)
+    response = Relationship("Response", back_populates="search", uselist=False)
 
-    #https://developers.google.com/youtube/v3/docs/search/list?hl=de
-    #requried
-    part: str = "snippet"
-    #filter
-    forContentOwner: bool = None
-    forDeveloper: bool = None
-    forMine: bool = None
-    relatedToVideoId: str = None
-    #optional
-    channelId: str = None
-    channelType: str = None
-    eventType: str = None
-    location: str = None
-    locationRadius: str = None
-    maxResults: int = None
-    onBehalfOfContentOwner: str = None
-    order: str = None
-    pageToken: str = None
-    publishedAfter: str = None
-    publishedBefore: str = None
-    q: str = None
-    regionCode: str = None
-    relevanceLanguage: str = None
-    safeSearch: str = None
-    topicId: str = None
-    type: str = None
-    videoCaption: str = None
-    videoCategoryId: str = None
-    videoDefinition: str = None
-    videoDimension: str = None
-    videoDuration: str = None
-    videoEmbeddable: str = None
-    videoLicense: str = None
-    videoSyndicated: str = None
-    videoType: str = None
-
-    response: Response = None
-
+    def __init__(self, part, forContentOwner=None, forDeveloper=None, forMine=None, relatedToVideoId=None, channelId=None, channelType=None, eventType=None, 
+                location=None, locationRadius=None, maxResults=None, onBehalfOfContentOwner= None, order=None, pageToken=None, publishedAfter=None, publishedBefore=None, 
+                q=None, regionCode=None, relevanceLanguage=None, safeSearch=None, topicId=None, type=None, videoCaption=None, videoCategoryId=None, videoDefinition=None, 
+                videoDimension=None, videoDuration=None, videoEmbeddable=None, videoLicense=None, videoSyndicated=None, videoType=None, response=None):
+        self.part=part
+        self.forContentOwner = forContentOwner
+        self.forDeveloper = forDeveloper
+        self.forMine = forMine
+        self.relatedToVideoId = relatedToVideoId
+        self.channelId = channelId
+        self.channelType = channelType
+        self.eventType = eventType
+        self.location = location
+        self.locationRadius = locationRadius
+        self.maxResults = maxResults
+        self.onBehalfOfContentOwner = onBehalfOfContentOwner
+        self.order = order
+        self.pageToken = pageToken
+        self.publishedAfter = publishedAfter
+        self.publishedBefore = publishedBefore
+        self.q = q
+        self.regionCode = regionCode
+        self.relevanceLanguage = relevanceLanguage
+        self.safeSearch = safeSearch
+        self.topicId = topicId
+        self.type = type
+        self.videoCaption = videoCaption
+        self.videoCategoryId = videoCategoryId
+        self.videoDefinition = videoDefinition
+        self.videoDimension = videoDimension
+        self.videoDuration = videoDuration
+        self.videoEmbeddable = videoEmbeddable
+        self.videoLicense = videoLicense
+        self.videoSyndicated = videoSyndicated
+        self.videoType = videoType
+        self.response = response
 
     def getSearchParameter(self):
         self_attrs = vars(self)
@@ -182,6 +216,8 @@ class Search:
             del not_none_params['id']
         if 'repsonse' in not_none_params:
             del not_none_params['response']
+        if '_sa_instance_state' in not_none_params:
+            del not_none_params['_sa_instance_state']
         return not_none_params
 
     
@@ -194,12 +230,12 @@ class Search:
         request_response = search_request.execute()
         self.response = Response(**request_response)
 
-        # Persists in SQL session
-        session.add(self.response)
-        session.commit()
-
 
 Base.metadata.create_all(bind=engine)
-search = Search(q="airport ambiance noise", type = "video", eventType = "completed")
+#TODO steer search
+search = Search(part="snippet", q="airport ambiance noise", type = "video", eventType = "completed")
+session.add(search)
 search.execute()
+
+session.commit()
 session.close()
