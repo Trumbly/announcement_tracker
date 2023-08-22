9436a0a5ed14511d92a0fd69bf5b8e2318d74e22
HannesLadendorf <hannes.ladendorf@gmx.de>
2023-08-22 21:54:25 +0200

    my current files

--- python/youtube_download.py
+++ python/youtube_download.py
@@ -1,26 +1,36 @@
 from __future__ import unicode_literals
 import os
-import youtube_dl
 from pathlib import Path
-# import youtube_api
+import youtube_api
+import youtube_dl
 
-# session = youtube_api.session
+session = youtube_api.session
 
 #TODO steer search
 # search = youtube_api.Search(part="snippet", q="airport ambiance noise", type = "video", eventType = "completed")
 # session.add(search)
 # search.execute()
+# session.commit()
+session.close()
 #TODO get video ids
 
 #download youtube video
-videoId = "p7eeZz_t4QE"  #TODO insert videoId from youtube search request
+video_id = "KJ7ZLeWxo2A"  #TODO insert videoId from youtube search request
+video_ids = [
+    '9JFC1xzdMx0',
+    'Z2CD4OyIM8U',
+    'cZa7jamQSuM',
+    'AtMzngYdx4s',
+    'jBcyGEEZqTA']
+video_urls=[f"https://www.youtube.com/watch?v={id}" for id in video_ids]
+
+video_url = f'https://www.youtube.com/watch?v={video_id}'
 file_path = Path(os.path.realpath(__file__))
 download_path = f'{file_path.parent.parent.absolute()}/data/yoututbe/'
 
 ydl_opts = {'format': 'bestaudio/best', 'outtmpl': f"{download_path}%(id)s.%(ext)s"}
 # on error: editing file: your/path/to/site-packages/youtube_dl/extractor/youtube.py Line 1794 and add the option fatal=False
 # https://stackoverflow.com/questions/75495800/error-unable-to-extract-uploader-id-youtube-discord-py
-youtube_dl.YoutubeDL(ydl_opts).download(url_list=[f'https://www.youtube.com/watch?v={videoId}'])
-
-# session.commit()
-# session.close()
+#TODO error handling
+for u in video_urls:
+    youtube_dl.YoutubeDL(ydl_opts).download(url_list=[u])
